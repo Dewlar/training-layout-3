9fe2a76074620a874c3531f88211b982ca0ad664
Andrei Markouski <insawm@gmail.com>
2021-08-23 21:25:39 +0300

    optimizing gulp, files and folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,38 @@
+let project_folder = "dist";
+let source_folder = "src";
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    img: project_folder + "/images/",
+  },
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: [
+      source_folder + "/scss/styles.scss",
+      "!" + source_folder + "/_*.scss",
+    ],
+    img: source_folder + "/images/**/*.{jpg,png,svg,gif,ico,webp}",
+  },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    img: source_folder + "/images/**/*.{jpg,png,svg,gif,ico,webp}",
+  },
+  clean: "./" + project_folder + "/",
+};
+
 let { src, dest } = require("gulp"),
   gulp = require("gulp"),
   browsersync = require("browser-sync").create(),
-  scss = require("gulp-sass")(require('sass'));
+  del = require("del"),
+  scss = require("gulp-sass")(require("sass"));
 
 function browserSync(params) {
   browsersync.init({
     server: {
-      baseDir: "./",
+      baseDir: "./" + project_folder + "/",
     },
     port: 3000,
     notify: false,
@@ -14,32 +40,43 @@ function browserSync(params) {
 }
 
 function html(params) {
-  return src('*.html')
+  return src(path.src.html)
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
+}
+
+function images(params) {
+  return src(path.src.img)
+    .pipe(dest(path.build.img))
     .pipe(browsersync.stream());
 }
 
 function css(params) {
-  return src('./styles.scss')
+  return src(path.src.css)
     .pipe(
       scss({
         outputStyle: "expanded",
       })
     )
 
-    .pipe(dest('./css'))
+    .pipe(dest(path.build.css))
     .pipe(browsersync.stream());
 }
 
 function watchFiles() {
-  gulp.watch(['*.html'], html);
-  gulp.watch(['./styles.scss'], css);
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.img], images);
 }
 
+function clean(params) {
+  return del(path.clean);
+}
 
-
-let build = gulp.series(gulp.parallel(html, css));
+let build = gulp.series(clean, gulp.parallel(css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.images = images;
 exports.css = css;
 exports.watch = watch;
 exports.default = watch;
